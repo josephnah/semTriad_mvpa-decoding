a4408765238b6b131253dcad7ac400fc413e50d3
Joseph Nah <joecool890@gmail.com>
2016-10-17 09:42:44 -0400

    added analysis file

---
+++ decodingMVPA.m
@@ -0,0 +1,121 @@
+% MVPA decoding script. Pretty much does what brainVoyager does but enables
+% batch processing, enabling easier data access/manipulation. Made to work 
+% with BV file outputs. Analysis is done % using the open source machine 
+% learning library LIBSVM (https://www.csie.ntu.edu.tw/~cjlin/libsvm/). 
+% Calls up VOI and leave one run out cross validation mvp files created
+% from brainVoyager. So does require some GUI based work. Future code might
+% change that..but this should work for now. Oh, also loops for total # of 
+% participants
+
+%% Cleanup before working
+clear all; clc;
+
+%% Participant Setup
+par         = {};
+% par{end+1}  = {'p01'}; % SHA
+% par{end+1}  = {'p02'}; % PSS
+par{end+1}  = {'p03'}; % AJC
+% par{end+1}  = {'p04'}; % NKC
+disp('start svm')
+root_dir    = '/Volumes/macLab/01_brainVoyager/semDuo';
+bothMean    = [];
+allMean     = [];
+
+% totalAcc    = [];
+totalInfo   = [];
+hem         = {'right','left'};
+
+allPars     = size(par,2);  % total number of participants
+allRuns     = 8;            % total number of runs
+allHems     = size(hem,2);  % only 2 hemispheres
+
+%% runs analysis for total number of participants
+for n = 1:allPars
+%     avgMean     = [];
+    par_dir     = fullfile(root_dir,par{n}{1}); % participant directory
+    voi_dir     = fullfile(par_dir,'stats');    % voi/stats directory
+    
+    
+    %% loop through hemisphere
+    for h = 1:allHems;
+        allMean = [];
+        cd(voi_dir); % cd to voi/stats dir
+        % vind voi file for hemisphere
+        if h == 1;
+            voiFile     = dir('evc_rh.voi');       % find voi file
+        elseif h == 2;
+            voiFile     = dir('evc_lh.voi');       % find voi file
+        end
+        
+        voi         = fullfile(voiFile.name);   % extract voi file name
+        curVoi      = BVQXfile(voi);            % load voiFile using BVQXfile f(x)
+        allVoi      = curVoi.NrOfVOIs;          % number of total VOIs in file
+        
+        %% loop through VOI
+        for v = 1:allVoi;
+            
+            learn       = 'Learn';
+            test        = 'Test';
+            
+            voiName     = curVoi(v).Name;
+            avgMean     = [];
+            %% loop through ROIs
+            for pairs = 1:4
+                pairNo = pairs*10;
+                totalInfo   = [];
+                totalAcc    = [];
+                for r = 1:allRuns;
+                    
+                    % get name of each run file. Change accordingly to fit your output
+                    runName     = 'RunLevelSplit-'; % damn you bv output..
+                    runNumber   = num2str(r);       % current run number (in string)
+                    runSearch   = strcat(runName,runNumber,'_'); % the name of file to search
+                    if h ==1
+                        mvpa_dir    = fullfile(par_dir,'stats','/mvpa/SR_left');    % voi/stats directory
+                        pairName    = 'SR_LEFT_';
+                    elseif h == 2
+                        mvpa_dir    = fullfile(par_dir,'stats','/mvpa/SR_right');    % voi/stats directory
+                        pairName    = 'SR_RIGHT_';
+                    end
+                    cd(mvpa_dir)
+                    % specify, locate, and extract train mvp file names
+                    trainFileName       = strcat(runSearch,learn,'_',voiName,'_',pairName,num2str(pairNo),'*');     % name of mvp file used for training
+                    mvpTrainFile        = dir(trainFileName);                       % find the mvp file
+                    mvpTrainFileName    = fullfile(mvpTrainFile.name);              % the name of mvp file
+                    bvTrainMVP          = BVQXfile(mvpTrainFileName);               % load mvpFile using BVQXfile f(x)
+                    classTrain          = bvTrainMVP.ClassValues;                   % get class labels (conditions)
+                    featTrain           = bvTrainMVP.FeatureValues;                 % get the value of features (voxels)
+                    sfeat               = sparse(featTrain);                        % sparse apart matrix "help sparse" for more info
+                    
+                    % specify, locate, and extract test mvp file names
+                    testFileName        = strcat(runSearch,test,'_',voiName,'_',pairName,num2str(pairNo),'*');   % name of mvp file used for training
+                    mvpTestFile         = dir(testFileName);                        % find the mvp file
+                    mvpTestFileName     = fullfile(mvpTestFile.name);               % the name of mvp file
+                    bvTestMVP           = BVQXfile(mvpTestFileName);                % load mvpFile using BVQXfile f(x)
+                    classTest           = bvTestMVP.ClassValues;                    % get class labels (conditions)
+                    featTest            = bvTestMVP.FeatureValues;                  % get the value of features (voxels)
+                    sfeatT              = sparse(featTest);                         % sparse apart matrix "help sparse" for more info
+                    
+                    % all this preparing...for these 2 goddamn lines. Two. God.
+                    % Damn. Lines. UGh. Anyays, utilizes LIBSVM as does BV,
+                    % hence needs to be installed to use.
+                    model               = svmtrain(classTrain,sfeat,'-t 0 -m 10000');            % train model, kernel = linear
+                    [plab,acc,dvpe]     = svmpredict(classTest,featTest,model,'-q');                 % test model
+                    
+                    %% try to print data into matrix for easy extraction
+                    fullInfo            = strcat(par{n},'_',runSearch,voiName);
+                    totalInfo           = [totalInfo, fullInfo];
+                    totalAcc            = [totalAcc, acc(1)] % <--------- WRONG WRONG WRONG 
+                    
+                end
+                avgMean = [avgMean mean(totalAcc)];
+            end
+            allMean = [allMean; avgMean];
+        end
+        bothMean = [bothMean allMean];
+    end
+%     parMean = [parMean bothMean];
+end
+
+
+mean(totalAcc(1,:))
