89f2c809e544fd35f5b053415732935483d80a4c
Joseph Nah <joecool890@gmail.com>
2016-10-22 19:33:13 -0400

    seems to be deadend unless i figure out vmp files

--- decodingMVPA.m
+++ decodingMVPA.m
@@ -17,97 +17,101 @@ disp('start svm')
 %% Participant Setup
 par         = {};
 par{end+1}  = {'p01'}; % SHA
-par{end+1}  = {'p02'}; % PSS
-par{end+1}  = {'p03'}; % AJC
-par{end+1}  = {'p04'}; % NKC
+% par{end+1}  = {'p02'}; % PSS
+% par{end+1}  = {'p03'}; % AJC
+% par{end+1}  = {'p04'}; % NKC
 allPars     = size(par,2);
 root_dir    = '/Volumes/macLab/01_brainVoyager/semDuo'; % root directory
-runSize     = 48; % # of trials
+vmpName     = '_SCCTBL_3DMCTS_THPGLMF2c_TAL_16-Conditions_GLM-2G_PreOn-1-PostCond-6_z-t_Trials.vmp';
+trialN      = 48; % # of trials
 
-% matrices of zeros for labeling trials
-allLabels   = [];% zeros(48,3,8);
-allData     = [];
-%% run for all participants
-for n = 1%:allPars
-    %% grabs labels for conditions you want to decode for all runs
-    runs = [];
-    for runIndex = 1:8
-        tempAllLabels   = [];
-        SR_RGHT_10 = [];
-        SR_RGHT_20 = [];
-        SR_RGHT_30 = [];
-        SR_RGHT_40 = [];
-        runN    = strcat('r',num2str(runIndex));
+objPairN    = {'10'; '20'; '30'; '40'}; 
+pairSide    = {'RGHT_' 'LEFT_'};    % len: 5
+pairCond    = {'SR_' 'NR_'};        % len: 3
+
+runN        = 8;
+pars = [];
+
+vmpTrainData = [];
+vmpTestData  = [];
+
+trainTestRuns = [1 2 3 4 5 6 7 8];
+
+%% load all VMPs for all participants and all runs
+for n = 1:allPars
+    for runIndex = 1:8 % loops through all runs
+        runNumb = strcat('r',num2str(runIndex));
         par_dir = fullfile(root_dir,par{n}{1});
-        run_dir = fullfile(par_dir,runN);
+        run_dir = fullfile(par_dir,runNumb);
         cd(run_dir)
-        vmpFile = strcat(runN,'_SCCTBL_3DMCTS_THPGLMF2c_TAL_16-Conditions_GLM-2G_PreOn-1-PostCond-6_z-t_Trials.vmp');
-        runs(runIndex).run = BVQXfile(vmpFile);
+        vmpFile = strcat(runNumb,vmpName);
+        pars(n).runs(runIndex).vmp = BVQXfile(vmpFile);
+    end
+end
+
+%% run analysis for all participants
+for n = 1:allPars
+    for pairIndex = 1:size(objPairN,1) % loops through pairs (10, 20, 30, 40)
         
-        %% checks label of trial and relabel in numbers
-        % labels = [condition hem type]
-        for trialIndex = 1:runSize;
-            
-            vmpName = runs(runIndex).run.Map(1,trialIndex).Name;
-            vmpData = runs(runIndex).run.Map(1,trialIndex).VMPData;
-            
-            nameChk = vmpName(1:10);
-            if nameChk == 'SR_RIGHT_1'
-                labels = [1 1 1];
-                SR_RGHT_10 = [SR_RGHT_10; trialIndex];
-            elseif nameChk == 'SR_RIGHT_2'
-                labels = [1 1 2];
-                SR_RGHT_20 = [SR_RGHT_20; trialIndex];
-            elseif nameChk == 'SR_RIGHT_3'
-                labels = [1 1 3];
-                SR_RGHT_30 = [SR_RGHT_30; trialIndex];
-            elseif nameChk == 'SR_RIGHT_4'
-                labels = [1 1 4];
-                SR_RGHT_40 = [SR_RGHT_40; trialIndex];
-            elseif nameChk == 'NR_RIGHT_1'
-                labels = [2 1 1];
-            elseif nameChk == 'NR_RIGHT_2'
-                labels = [2 1 2];
-            elseif nameChk == 'NR_RIGHT_3'
-                labels = [2 1 2];
-            elseif nameChk == 'NR_RIGHT_4'
-                labels = [2 1 4];
-                
-            elseif nameChk == 'SR_LEFT_10'
-                labels = [1 2 1];
-            elseif nameChk == 'SR_LEFT_20'
-                labels = [1 2 2];
-            elseif nameChk == 'SR_LEFT_30'
-                labels = [1 2 3];
-            elseif nameChk == 'SR_LEFT_40'
-                labels = [1 2 4];
-            elseif nameChk == 'NR_LEFT_10'
-                labels = [2 2 1];
-            elseif nameChk == 'NR_LEFT_20'
-                labels = [2 2 2];
-            elseif nameChk == 'NR_LEFT_30'
-                labels = [2 2 3];   
-            elseif nameChk == 'NR_LEFT_40'
-                labels = [2 2 4];
+        classIndex   = [];
+        for trialIndex = 1:trialN % loops through 48 trials in 1 run
+            classLabl = pars(n).runs(1).vmp.Map(1,trialIndex).Name;
+            % find appropriate trials based on hemisphere
+            if pairIndex < size(objPairN,1)/2      % for Left Hem ROIs %%%%%%%%%%%%%%%% WRONG
+                curLabl = strcat(pairSide(1),objPairN(pairIndex));
+            elseif pairIndex >= size(objPairN,1)/2 % for right Hem ROIs
+                curLabl = strcat(pairSide(2),objPairN(pairIndex));
             end
             
-            tempAllLabels = [tempAllLabels; labels];
+            % gets trial # of relevant classes for train/test matrix
+            if strcmp(curLabl,classLabl(4:10))
+                classIndex = [classIndex; trialIndex];
+            end
         end
-        allLabels(:,:,runIndex) = tempAllLabels;
+                        
+        % loop through runs to grab train/test data
+        for runIndex = 1:runN       % loops through runs (1 ~ 8), runIndex = test run
+            
+            testRun     = runIndex;
+            trainRun    = setdiff(trainTestRuns,testRun);  % selects the testing runs: n = 7
+            
+            % loop through test runs and grab relevant data
+            for testDataRun = 1:size(classIndex,1);
+                vmpTestData = pars(n).runs(testRun).vmp.Map(1,classIndex).VMPData;
+            end
+            trainLablIndex = [];            
+        end
+        d
+        %% read in VOMs
+        vom_dir = fullfile(par_dir,'vom'); % voi/stats directory
+        cd(vom_dir);
+        
+        vom     = dir('p01-*'); % finds all VOM files
+        vomN    = size(vom,1); % # of VOIs (should be 14)
+        
+        %% run for all ROIs
+        for vomIndex = 1:vomN
+            curVOM  = BVQXfile(vom(vomIndex).name);
+            vomVoxel                        = curVOM.VOM.Voxels;        % Get the x,y,z coordinates of all the voxels
+            vomData.Clust(vomIndex).Name    = myVOIs(vomIndex).Name;    % Set the VOIBetas.Clust(j) name as the given voi name.
+            [voiVoxelSize, columns]         = size(vomVoxel);           % # of voxels in the clust; x, y, z coordinates
+            
+            for voxel = 1:voiVoxelSize
+                x = voiVoxel(voxel,1);
+                y = voiVoxel(voxel,2);
+                z = voiVoxel(voxel,3);
+                
+                % convert the coordinates of the voxels (x,y,z) to
+                % matlab array indices (Mx, My, Mz)using Tal2Matlab
+                [Mx,My,Mz] = Tal2Matlab(x,y,z);
+                
+            end
+        end
+
+        d
     end
+
     
-    %% read in VOI
-    voi_dir = fullfile(par_dir,'rois'); % voi/stats directory
-    cd(voi_dir);
-    voi     = BVQXfile('allHem.voi'); % need to compile voi file with all VOIs order: left -> right
-    myVOIs  = voi.VOI;      % 
-    voiN    = voi.NrOfVOIs; % # of VOIs
-    
-    %% run for all ROIs
-    for voiIndex = 1:voiN
-        voiVoxel                        = myVOIs(voiIndex).Voxels;  % Get the x,y,z coordinates of all the voxels
-        voiBetas.Clust(voiIndex).Name   = myVOIs(voiIndex).Name;    % Set the VOIBetas.Clust(j) name as the given voi name.
-        [voiVoxelSize, columns]         = size(voiVoxel);           % # of voxels in the clust; x, y, z coordinates
         
         for pairIndex = 1%:4
             classValuesRun = [];
@@ -117,7 +121,7 @@ for n = 1%:allPars
                 for trialIndex = 1:size(allLabels,1) % runs through all trials
                     curLabels = allLabels(:,:,runIndex);
                     
-                    if voiIndex < voiN/2 % for the leftHem ROIs
+                    if vomIndex < voiN/2 % for the leftHem ROIs
                         %% extract relevant labels and designate correct labels. This is where changes might have to be made for different decoding comparisons
                         % if just comparing 2, labels: 1, -1 [check: 2016.10.20]
                         % if multi, just in numbers
@@ -128,7 +132,7 @@ for n = 1%:allPars
                             classValues = -1;
                         end
                         
-                    elseif voiIndex >= voiN/2 % for the rghtHem ROIs
+                    elseif vomIndex >= voiN/2 % for the rghtHem ROIs
                         if curLabels(trialIndex,:) == [1 2 pairIndex];      % SR_LEFT*0
                             classValues = 1;
                         elseif curLabels(trialIndex,:) == [2 2 pairIndex];  % NR_LEFT_*0
@@ -140,7 +144,7 @@ for n = 1%:allPars
                 d
             end
         end
-    end
+    
 end
 d
 
@@ -161,7 +165,7 @@ allHems     = size(hem,2);  % only 2 hemispheres
 for n = 1:allPars
 %     avgMean     = [];
     par_dir     = fullfile(root_dir,par{n}{1}); % participant directory
-    voi_dir     = fullfile(par_dir,'stats');    % voi/stats directory
+    vom_dir     = fullfile(par_dir,'stats');    % voi/stats directory
     d
     bothHemMean     = [];
     allMeanROI      = [];
@@ -169,7 +173,7 @@ for n = 1:allPars
     %% loop through hemisphere
     for h = 1:allHems;
         allMeanROI = [];
-        cd(voi_dir); % cd to voi/stats dir
+        cd(vom_dir); % cd to voi/stats dir
         % vind voi file for hemisphere
         if h == 1;
             voiFile     = dir('evc_rh_combDV.voi');       % find voi file
@@ -248,3 +252,111 @@ end
 
 
 mean(allRunAcc(1,:))
+
+% Input VOI and vmp files from trial estimation
+
+rootDir = '/Users/joecool890/Dropbox/MATLAB/GWU/imaging/00_MVPA/02_SVM/fromFatma/';
+cd(rootDir);
+
+Experiment=[];
+Experiment(1).run=BVQXfile('JL_r1_SCCAI2_3DMCTS_THPGLMF2c_TAL_Trials.vmp');
+Experiment(2).run=BVQXfile('JL_r2_SCCAI2_3DMCTS_THPGLMF2c_TAL_Trials.vmp');
+Experiment(3).run=BVQXfile('JL_r3_SCCAI2_3DMCTS_THPGLMF2c_TAL_Trials.vmp');
+Experiment(4).run=BVQXfile('JL_r4_SCCAI2_3DMCTS_THPGLMF2c_TAL_Trials.vmp');
+
+
+%now read in voi data
+myVOI=BVQXfile('JL_LH_complete.voi');
+myVOIs = myVOI.VOI;
+nVOI=myVOI.NrOfVOIs
+
+block=1;
+L=1; % block and L are the same numberS)
+for j = 1:1 %analysis for 1 ROI
+    [A,B]=size(myVOIs(j).Voxels);
+    coordinations= myVOIs(j).Voxels;
+    %store coordinates, name and size for each voi
+    Alldata=zeros(48,A);
+    Alllabels=zeros(48,1);
+    for r = 1:4
+        %loops through all runs
+        for b=1:12
+            %loops thru all trials
+            data=Experiment(r).run.Map(1,b).VMPData;
+            %check label
+            name=Experiment(r).run.Map(1,b).Name;
+            k=name(1:6);
+            if k=='LeftFa'
+                Alllabels(L,1)=1;
+            elseif k=='RightF'
+                Alllabels(L,1)=2;
+            elseif k=='LeftHo'
+                Alllabels(L,1)=3;
+            elseif k=='RightH'
+                Alllabels(L,1)=4;
+            end
+            
+            
+            for v = 1:A %loops through all voxels in a VOI
+                x= coordinations(v,1);
+                y= coordinations(v,2);
+                z= coordinations(v,3);
+                
+                [Mx,My,Mz] = Tal2Matlab(x,y,z);
+                Alldata(block, v)=data(Mx,My,Mz);
+                
+            end
+            block=block+1;
+            L=L+1;
+            
+            
+        end
+    end
+    
+    clear coordinations
+    %end of voi-clear for next
+end
+
+T=1;
+for i=1:4
+    for k=1:4
+        if k~=i
+            for z=1:12
+                
+                trainlabels(T,:)=Alllabels((k-1)*12+z,:);
+                traindata(T,:)=Alldata((k-1)*12+z,:);
+                T=T+1;
+            end
+        end
+    end
+    opt.kerntype={'linear'};
+    opt.ncross=1;
+    opt.svmtype='C';
+    svm.cost=2.71;
+    model=svmtrain(trainlabels, traindata,opt)
+    
+    TT=1;
+    for z=1:12
+        
+        testdata(TT,:)=Alldata((i-1)*12+z,:);
+        testlabels(TT,:)=Alllabels((i-1)*12+z,:);
+        TT=TT+1;
+    end
+    opt.ncross=1;
+    [plab,acc,dvpe]=svmpredict(testlabels,testdata,model,opt)
+    
+    SVM(i).trainmat=traindata;
+    SVM(i).trainlabel=trainlabels;
+    SVM(i).testmat=testdata;
+    SVM(i).testlabel=testlabels;
+    SVM(i).predictions=plab;
+    SVM(i).accuracy=acc;
+    SVM(i).decision=dvpe;
+    SVM(i).Model=model;
+    
+    clear testdata
+    clear testlabels
+    clear trainlabels
+    clear traindata
+    
+end
